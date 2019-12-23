35f39672efc1312c0e7af312abfbef39920d543a
MXAnton <anton.lehrberg@hotmail.se>
2019-12-23 17:17:33 +0100

    Added a simple 2d raycast gun script and a barrel to shoot on

---
+++ Assets/Scripts/DestroyTimer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTimer : MonoBehaviour
+{
+    public float destroyTime = 5f;
+
+    private void Start()
+    {
+        StartCoroutine(DestroyObject());
+    }
+
+    IEnumerator DestroyObject()
+    {
+        yield return new WaitForSeconds(destroyTime);
+
+        Destroy(gameObject);
+    }
+}
