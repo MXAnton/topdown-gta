8947b2475527d67f9443cd7875675e127689dcf3
MXAnton <anton.lehrberg@hotmail.se>
2019-12-29 11:03:52 +0100

    Added simple system for the player to get in and out from vehicle

---
+++ Assets/Scripts/Player/GetInOutVehicle.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetInOutVehicle : MonoBehaviour
+{
+    public Transform raycastPosition;
+    public float range = 1f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            CheckIfInVehicle();
+        }
+    }
+
+    void CheckIfInVehicle()
+    {
+        if (transform.parent == null)
+        {
+            FindVehicleDoor();
+        }
+        else if (transform.parent.gameObject.tag == "VehicleDoor")
+        {
+            GetOutFromVehicle();
+        }
+    }
+
+    void FindVehicleDoor()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(raycastPosition.position, transform.up, range);
+
+        if (hitInfo && hitInfo.transform.gameObject.tag == "VehicleDoor")
+        {
+            //Debug.Log("Found a vehicle door");
+
+            VehiclePlayers vehiclePlayers = hitInfo.transform.GetComponentInParent<VehiclePlayers>();
+
+            if (vehiclePlayers)
+            {
+                vehiclePlayers.TryToSitInVehicle(gameObject, hitInfo.transform.GetComponent<VehicleDoorInfo>().vehicleDoorNumber);
+            }
+        }
+        //else
+        //{
+        //    Debug.Log("Didn't find any vehicle door");
+        //}
+    }
+
+    void GetOutFromVehicle()
+    {
+        //Debug.Log("Get out from vehicle");
+
+        GetComponentInParent<CarController>().enabled = false;
+
+        transform.parent = null;
+
+        GetComponentInChildren<Animator>().enabled = true;
+        GetComponentInChildren<SpriteRenderer>().enabled = true;
+
+        GetComponent<PlayerMovement>().enabled = true;
+        GetComponent<Collider2D>().enabled = true;
+    }
+}
---
+++ Assets/Scripts/Vehicles/VehiclePlayers.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehiclePlayers : MonoBehaviour
+{
+    public GameObject[] vehiclePositions;
+
+    public void TryToSitInVehicle(GameObject player, int position)
+    {
+        if (vehiclePositions[position].transform.childCount == 0)
+        {
+            //Debug.Log("Get in vehicle");
+
+            player.GetComponentInChildren<Animator>().enabled = false;
+            player.GetComponentInChildren<SpriteRenderer>().enabled = false;
+
+            player.GetComponent<PlayerMovement>().enabled = false;
+            player.GetComponent<Collider2D>().enabled = false;
+
+            player.transform.parent = vehiclePositions[position].transform;
+            player.transform.localPosition = new Vector2(0, 0);
+            player.transform.localEulerAngles = new Vector3(0, 0, 0);
+
+            if (vehiclePositions[0].transform.childCount != 0)
+            {
+                GetComponent<CarController>().enabled = true;
+            }
+        }
+        //else
+        //{
+        //    Debug.Log("Position is not empty");
+        //}
+    }
+}
